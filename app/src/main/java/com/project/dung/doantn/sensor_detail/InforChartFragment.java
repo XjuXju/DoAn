5bc115bc3b7643e5c84592b980808dc39bc8483b
Trần Dung <tranthidung.bk@gmail.com>
2017-04-15 17:01:12 +0700

    chart

--- app/src/main/java/com/project/dung/doantn/sensor_detail/InforChartFragment.java
+++ app/src/main/java/com/project/dung/doantn/sensor_detail/InforChartFragment.java
@@ -14,21 +14,62 @@ import android.widget.RelativeLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
 
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.project.dung.doantn.R;
 
+import java.util.ArrayList;
+
 public class InforChartFragment extends Fragment {
-    RelativeLayout mainLayout;
-    LineChart mChart;
+
 
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.chart_activity, container, false);
-        mainLayout = (RelativeLayout) view.findViewById(R.id.chartView);
-        //create chart and add into layout
+        LineChart lineChart = (LineChart) view.findViewById(R.id.chart);
 
 
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+        entries.add(new Entry(4f, 6));
+        entries.add(new Entry(8f, 7));
+        entries.add(new Entry(6f, 8));
+        entries.add(new Entry(2f, 9));
+        entries.add(new Entry(18f,10));
+        entries.add(new Entry(9f, 11));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        LineData data = new LineData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        dataset.setDrawCubic(true);
+        dataset.setDrawFilled(true);
+
+        lineChart.setData(data);
+        lineChart.animateY(5000);
         return view;
     }
 
